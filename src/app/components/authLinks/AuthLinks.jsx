82385c861b43988aa5f2f3be9d2fda692bda1ad1
Hardik <hardiknandwani420@gmail.com>
2024-01-21 15:12:53 +0530

    added initial files

---
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './authlinks.module.css';
+const AuthLinks = () => {
+
+    const status = "not"
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default AuthLinks
